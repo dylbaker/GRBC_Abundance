e1b6f02d82466d8f7ccc9bcfc9f3f3eea627bdfb
Dylan Baker <bakerdyl@umich.edu>
2022-11-10 15:48:28 -0500

    Finalized plot script

--- plot_functions.R
+++ plot_functions.R
@@ -1,41 +1,65 @@
 
 
-abundance_plot_isolate <- function(species_name, data, color_data, bar_color, genus_species) {
-  plot <- data %>%
-    filter(host_species == species_name) %>%
-    #Reorder genus by average relative abundance to get a smooth curve
-    ggplot( aes(x = genus, y = avg_rel_abund, fill = color)) +
-    geom_bar(stat = "Identity") +
-    theme_minimal() +
-    scale_fill_manual(values = c(bar_color, "black"), name =  '', labels = c("Cultured Isolate", "Not Isolated")) +
+abundance_plot_isolate <- function(species_name, data, bar_color, genus_species) {
+  plot <- data |>
+    filter(host_species == species_name) |>
+    mutate(subset = ifelse(rel_abund > 0.01, "Abundant","Rare (<1% of Total Reads)")) |>
+    #Reorder asv by relative abundance to get a smooth curve
+    ggplot(aes(x = reorder(asv, -rel_abund), y = rel_abund, fill = color)) +
+    geom_text(aes(label = significant), 
+              position = position_dodge(width = .9), hjust = -0.2, vjust = .7, size = 20 / .pt) +
+    geom_col() +
+    coord_flip() +
+    facet_wrap( ~ subset, scales = "free_x") +
+    theme_pubclean() +
+    scale_fill_manual(values = c(bar_color, "black"), name =  '', labels = c("ASVs with Cultured Representatives from Phycosphere", "ASVs not Cultured from Phycosphere")) +
     guides(fill = guide_legend(
       title = substitute(paste(italic(genus_species))),
       title.position = "top",
       nrow = 2, byrow = TRUE
     )) +
-    labs( y = "Relative Abundance (Genus)", x = "", fill = '',
-          title = "") +
-    scale_y_continuous("Average Relative Abundance", limits = c(0, 0.32), breaks = c(0.0, 0.1, 0.2, 0.3)) +
-    theme(axis.title.x = element_text(size = 12),
-          axis.text.x = element_text(angle = 70, hjust = 1, size = 14,
-                                     color = color_data$color),
-          panel.background = element_rect(fill = "transparent"), # bg of the panel
-          plot.background = element_rect(fill = "transparent", color = NA), # bg of the plot
-          panel.grid.major = element_blank(), # get rid of major grid
-          panel.grid.minor = element_blank(), # get rid of minor grid
-          legend.background = element_rect(fill = "transparent"), # get rid of legend bg
-          legend.box.background = element_rect(fill = "transparent"),
-          axis.title.y = element_text(size = 10),
-          axis.text.y = element_text(size = 10),
-          legend.position = "bottom",
-          legend.box.just = "center",
-          legend.direction = "vertical",
-          legend.title = element_text(vjust = .5, size = 12),
-          legend.text = element_text(vjust = .5, size = 12),
-          plot.title = element_text(hjust = .5, size = 16)) +
-    scale_x_reordered()
+    labs( y = "Relative Abundance (ASV)", x = "Amplicon Sequence Variant", fill = '',
+          title = "") 
   
   return(plot)
+}
+
+abundance_plot_combined <- function(species_name, data, data2, bar_color, genus_species) {
+  plot1 <- data |>
+    filter(host_species == species_name) |>
+    #Reorder asv by relative abundance to get a smooth curve
+    ggplot(aes(x = reorder(asv, -rel_abund), y = rel_abund, fill = class)) +
+    scale_fill_brewer() +
+    geom_text(aes(label = significant), 
+              position = position_dodge(width = .9), hjust = -0.2, vjust = .7, size = 20 / .pt) +
+    geom_col() +
+    coord_flip() +
+    theme_pubclean() +
+    ylim(0.00,0.21) +
+    labs( y = "", x = "Amplicon Sequence Variant", fill = '',
+          title = "Phycosphere of a Green Alga 3 Days Post Introduction to Pond Water") 
+  
+  plot2 <- data2 |>
+    filter(host_species == species_name) |>
+    #Reorder asv by relative abundance to get a smooth curve
+    ggplot(aes(x = reorder(asv, -rel_abund), y = rel_abund, fill = class)) +
+    scale_fill_brewer() +
+    geom_text(aes(label = significant), 
+              position = position_dodge(width = .9), hjust = -0.2, vjust = .7, size = 20 / .pt) +
+    geom_col() +
+    coord_flip() +
+    theme_pubclean() +
+    ylim(0.00,0.21) +
+    labs( y = "Relative Abundance (ASV)", x = "Amplicon Sequence Variant", fill = '',
+          title = "Phycosphere of a Green Alga 31 Days Post Introduction to Pond Water") 
+  
+  patchwork <- (plot1 / plot2) 
+  
+  final_plot = patchwork +
+    plot_layout(guides = "collect") +
+    plot_annotation(tag_levels = 'A') & theme(legend.position = "bottom")
+    
+  return(final_plot)
 }
 
 #Colors used for phylum classification
